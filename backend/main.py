b4c1494807c91510006b7f72c3b7912aa2e03fbd
a9mansoo <a9mansoo@uwaterloo.ca>
2026-05-18 22:16:47 -0400

    test

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,10 @@ def main():
     app_config = generate_app_config(
         root_path=os.getenv("ROOT_PATH"), app_version=os.getenv("APP_VERSION")
     )
-    app = create_app(app_config)
+    app = create_app(             
+        
+        
+        app_config)
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("SERVER_PORT", 8000)))
 
 
